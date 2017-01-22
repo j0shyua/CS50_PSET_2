958b995150f374f010db9e03ba7df1707c259101
j0shyua <joshua2002yu@gmail.com>
2017-01-21 18:59:37 -0800

    Added initials.c
    Allows user to input their name, and prints out their initials in terminal.

---
+++ initials.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+
+int main (void)
+{
+    char* name = GetString();
+    printf ("%c", toupper (name [0]));
+            
+    for (int i = 0, n = strlen(name); i < n; i++)
+    {
+        if (name[i] == ' ')
+        {
+            printf ("%c", toupper (name [i+1]));
+        }
+    }
+    
+    printf ("\n");
+}
